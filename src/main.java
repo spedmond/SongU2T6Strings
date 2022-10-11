e567efefdbeabd752a6045d15374785efc3239a7
unknown <BT_3W2_25@BKTECH.LOCAL>
2022-10-11 12:16:32 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        String greeting = "Hello, friend!\n";
+        System.out.print(greeting);
+        System.out.print("Nice to see you!\n");
+        System.out.println("Goodbye!");
+    }
+}
